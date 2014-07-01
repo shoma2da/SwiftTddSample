924575d26f04b7a78f6c39786e6ffb2a5b0b1d11
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-02 06:26:26 +0900

    FizzBuzzクラスの存在確認テスト

---
+++ TddSampleTests/FizzBuzzTest.swift
@@ -0,0 +1,18 @@
+//
+//  FizzBuzzTest.swift
+//  TddSample
+//
+//  Created by Shoichi Matsuda on 2014/07/02.
+//  Copyright (c) 2014年 shoma2da. All rights reserved.
+//
+
+import XCTest
+import TddSample
+
+class FizzBuzzTest: XCTestCase {
+
+    func testExistFizzBuzz() {
+        XCTAssertNotNil(FizzBuzz())
+    }
+
+}
