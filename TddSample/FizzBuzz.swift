e0098b2781d29b6732ca6e3d831d3dab0cc890c5
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-02 06:51:41 +0900

    5の倍数だった場合にBuzzに変換されるかどうかのテストを成功させる

--- TddSample/FizzBuzz.swift
+++ TddSample/FizzBuzz.swift
@@ -14,6 +14,9 @@ class FizzBuzz {
         if (number % 3 == 0) {
             return "Fizz"
         }
+        if (number % 5 == 0) {
+            return "Buzz"
+        }
         return String(number)
     }
     
