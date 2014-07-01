6368244f7047d1b1fa3d5c97ca3533d677a559cd
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-07-02 06:27:42 +0900

    FizzBuzzクラスの存在確認テストを成功させる(リファクタリングなし)

---
+++ TddSample/FizzBuzz.swift
@@ -0,0 +1,13 @@
+//
+//  FizzBuzz.swift
+//  TddSample
+//
+//  Created by Shoichi Matsuda on 2014/07/02.
+//  Copyright (c) 2014年 shoma2da. All rights reserved.
+//
+
+import Foundation
+
+class FizzBuzz {
+    
+}
